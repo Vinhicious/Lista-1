781b6a629d6abee0a905d75a1d26b7ad3fd08e32
Vinicios Trindade Costa <135274652+Vinhicious@users.noreply.github.com>
2025-03-31 09:28:05 -0300

    Update 8.py

--- Novas Tecnologias/Lista 2/8.py
+++ Novas Tecnologias/Lista 2/8.py
@@ -2,9 +2,11 @@ n = int(input("Digite a Posição da sequencia de Fibonacci: "))
 
 if n <= 0:
     print("Posição inválida")
+elif n == 1:
+    print(f"Sequencia de Fibonacci até {n}: \n [1]")
 else:
     fibo = [1,1]
     while len(fibo)<n:
         fibo = fibo + [fibo[-1]+fibo[-2]]
 
-print(f"Sequencia de Fibonacci {n}:", fibo)
+    print(f"Sequencia de Fibonacci {n}:", fibo)
